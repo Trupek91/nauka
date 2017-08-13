3332f8b0af10da4df297b6375c48fa30f5df2e09
Trupek <dariusz.m123@gmail.com>
2017-08-13 23:37:26 +0200

    opisalem komendy wyswietlania, dodawania itd

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
-packae main
+package main // paczka komend golang
 
+import "fmt" //komenda do importowania paczki
 
-func main(){
+var trupka = "Trupek"
+
+// fmt moze byc zastapione yy (zapomnialem w jakiej sytuacji)
+// Test jest zmienna ktora pomaga okreslic cos
+const Test = "Pogromca, Yakuzy"
+const jedynka = 6
+
+// const nie pozwala nadpisac zmiennej
+func main() { // glowna funkcja ktora zawsze jest wyswietlana i wykonywana
+	var niraziel string // var okresla zmienna a string definiuje zmienna jako litery
+	niraziel = "niraziel"
+	fmt.Println(niraziel)
+	fmt.Println(trupka, Test)
+	fmt.Println(dodawanie(3, 1))
+}
+
+// dowanie(a,b)  a ib to arugmenty i musza byc zawsze podane
+// dodawanie()
+func dodawanie(a int, b int) (int, int) { // int okresla zmienna jako liczbe
+	a = a * 2
+	b = b * 2
+	return a, b
+}
+
+func dodowanieBezWracania(a int, b int) int {
+	fmt.Println(a + b) // funkcja do wyswietlania
+	fmt.Println("Test pod koniec funkcji", Test)
+	return a + b // zadanie co ma funkcja zwrocic po wykonaniu czynnosci
 }
